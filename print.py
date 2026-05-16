02e66bdd717217cf8d352d3d01360dbe1986b1d4
yuvaanhelps <yuvaan@jethi.com>
2026-05-16 10:43:57 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,8 @@
+# printing a greeting message
+print("hello world")
+# printing my name
+print("my name is yuvaan")
+# printing a fun fact about myself
+print("i love to play football")
+# performing and printing simple calculations
+print("the result of 3 + 5 is:", 3 + 5)
