ce2614b0086a5eaa4eb690f8159d159de388d674
yuvaanhelps <yuvaan@jethi.com>
2026-05-16 10:59:01 +0530

    Add files via upload

---
+++ my routine.py
@@ -0,0 +1,13 @@
+print("My Daily Routine")
+# Step 1: Waking up
+print("I wake up at 7 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8 AM.")
+
+# Step 3: Playing after school
+print("I play with my friends in the evening.")
+
+# Step 4: Studying
+print("I study for 2 hours at night.")
+print("Total study hours in a week:", 2 * 7)
