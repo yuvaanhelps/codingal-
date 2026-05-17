8d729e2f05aebbc07f22bc191d17c427cd5ecd4b
yuvaanhelps <yuvaan@jethi.com>
2026-05-17 13:15:55 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,4 @@
+import keyword
+# print all the python keywords
+print("python keyword are...\n")
+print(keyword.kwlist)
---
+++ world.py
@@ -0,0 +1,10 @@
+# print a sentence
+print("welcome to the world of programing")
+# print a number
+print(4)
+# next line after print
+print("hello python \n")
+# pass multiple arguments for printing
+print("hello",5)
+# end argument of statement
+print("welcome to ", end= "*")
