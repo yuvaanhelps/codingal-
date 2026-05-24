78aca52925dbc00a423e546759f2e58e02a08007
yuvaanhelps <yuvaan@jethi.com>
2026-05-24 10:03:30 +0530

    Add files via upload

---
+++ storing birthday.py
@@ -0,0 +1,23 @@
+# Birthday list
+
+name1 = input("Enter first name: ")
+birthday1 = input("Enter first birthday: ")
+
+name2 = input("Enter second name: ")
+birthday2 = input("Enter second birthday: ")
+
+name3 = input("Enter third name: ")
+birthday3 = input("Enter third birthday: ")
+
+name4 = input("Enter fourth name: ")
+birthday4 = input("Enter fourth birthday: ")
+
+name5 = input("Enter fifth name: ")
+birthday5 = input("Enter fifth birthday: ")
+
+print("\nBirthdays")
+print(name1, ":", birthday1)
+print(name2, ":", birthday2)
+print(name3, ":", birthday3)
+print(name4, ":", birthday4)
+print(name5, ":", birthday5)
