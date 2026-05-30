04967d8bfff8fe972f664f09d07275d90ba9805b
yuvaanhelps <yuvaan@jethi.com>
2026-05-30 20:48:34 +0530

    Add files via upload

---
+++ summers.py
@@ -0,0 +1,6 @@
+temperature = float(input("Enter the temperature (°C): "))
+
+if temperature >= 20:
+    print("Suitable for light clothes.")
+else:
+    print("Wear warm clothes such as a jacket or pullover.")
