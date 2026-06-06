44a564fb8798c81cab2bac3957ea66888414960f
yuvaanhelps <yuvaan@jethi.com>
2026-06-06 10:20:04 +0530

    Add files via upload

---
+++ identity.py
@@ -0,0 +1,13 @@
+x = 5.5
+if(type(x)is int):
+  print("true")
+else:
+  print("false")
+
+x = 20
+y = 20
+if(x is y):
+  print("x and y have same identity")
+y = 30
+if("x is not y"):
+  print("x and y have different identity")
