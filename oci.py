92e2d1215f8517953ce1e162d421a8f2cbd94f78
yuvaanhelps <yuvaan@jethi.com>
2026-06-07 09:57:20 +0530

    Add files via upload

---
+++ oci.py
@@ -0,0 +1,16 @@
+# ord() function returns ASCII value
+
+print(ord('A'))     # Output: 65
+
+print(ord('a'))     # Output: 97
+
+print(ord('0'))     # Output: 48
+
+print(ord('@'))     # Output: 64
+
+
+# chr() function converts ASCII back to character
+
+print(chr(65))      # Output: A
+
+print(chr(97))      # Output: a
