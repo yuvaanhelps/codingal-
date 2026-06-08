e0aff903a1a9f9e382ff3b6e70ea63b7110414ef
yuvaanhelps <yuvaan@jethi.com>
2026-06-08 20:18:42 +0530

    Add files via upload

---
+++ precedence.py
@@ -0,0 +1,15 @@
+v = 12
+w = 11
+x = 10
+y = 9
+z = 8
+z = (v+w) * x/y;
+print("value of (v+w) * x/y is", z)
+name = "alex"
+age = 0
+if name == "alex" or name =="john" and age >= 2:
+    print("hello! welcome")
+else:
+    print("goodbye")
+
+
