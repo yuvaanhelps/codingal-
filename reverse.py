58d4530112e2d1d21eac4057d40a9e2e44ce0984
yuvaanhelps <yuvaan@jethi.com>
2026-06-14 09:40:00 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,7 @@
+# input number greater than 1
+n= int(input("enter the value of n:"))
+# print the numbers from n to 1
+print("the numbers from{0} to{1}" .format(n,1))
+# loop to print numbers
+for i in range(n,0,-1):
+    print (i)
