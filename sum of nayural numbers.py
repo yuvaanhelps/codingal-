ae5f241955e12203a3384e43c56390cd9355fbd6
yuvaanhelps <yuvaan@jethi.com>
2026-06-20 10:17:10 +0530

    Add files via upload

---
+++ sum of nayural numbers.py
@@ -0,0 +1,9 @@
+#input the value of terms
+n = int(input("enter the value of terms"))
+sum = 0 #initialise
+i = 1 #initialise
+while i<=n: # loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+    print("\n sum =", sum)
+    
