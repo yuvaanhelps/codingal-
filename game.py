fb35c410f7b3b5f8b6d396a81d1f0261b93ec914
yuvaanhelps <yuvaan@jethi.com>
2026-07-11 10:45:37 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,20 @@
+
+print("welcome to number guessing game")
+for i in range:"1 to 50"
+secret=27
+print("you are correct!")
+secret=37
+print("you are warm")
+secret=47
+print("you are cold")
+secret=7
+print()
+secret=30
+print("you are warm")
+secret=32
+print("you are cold")
+secret=1
+print("you are ice cold")
+
+
+
