a3578306ecd593893bb55b1a0b56e93cae9e3ecc
yuvaanhelps <yuvaan@jethi.com>
2026-08-22 09:44:42 +0530

    Add files via upload

---
+++ bye bye.py
@@ -0,0 +1,12 @@
+valid= False
+while not valid : #using nested while loop
+ try:
+    n=int(input("enter a number:"))
+    #enter a even number
+    while n%2==0:
+        print("bye")
+    valid=True
+ except ValueError:
+    print("invalid")
+        
+
