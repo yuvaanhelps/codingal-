1ad9328b4e375e6920d4c4208a4a2813e8f87b30
yuvaanhelps <yuvaan@jethi.com>
2026-09-05 09:19:26 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+#create an empty list
+empty_list = []
+print()
+#a list of numbers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+#use * operator
+triples = [1,2,3] * 3
+print(triples)
+# reverse a list
+alist = [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist, "\n")
